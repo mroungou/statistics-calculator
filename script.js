54c4c76a9bd6c309611fb622a5edd4ec2d42f167
Malick Roungou <mjbroungou@gmail.com>
2024-10-23 20:57:02 -0400

    updates

--- script.js
+++ script.js
@@ -2,13 +2,20 @@ const calculate = () => {
     const value = document.querySelector('#numbers').value;
     const array = value.split(/,\s*/g)
     const numbers = array.map(el => Number(el)).filter(el => !isNaN(el))
+
+    const mean = getMean(numbers);
+    document.querySelector('#mean').textContent = mean;
 }
 
-const getMean = (array) => {
-    /* reduce(callBackFn, intialvalue) takes a callback fn and an intial value
-    the intial value in this case is set to 0 */
-    const sum = array.reduce((acc, el) => {acc + el}, 0)
-    /* dvsn by the array length to get the mean */
-    const mean = sum / array.length
-    return mean
-}
+// const getMean = (array) => {
+//     /* reduce(callBackFn, intialvalue) takes a callback fn and an intial value
+//     the intial value in this case is set to 0 */
+//     const sum = array.reduce((acc, el) => {acc + el}, 0)
+//     /* dvsn by the array length to get the mean */
+//     const mean = sum / array.length
+//     return mean
+// }
+
+// with implicit arrow functions
+
+const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
