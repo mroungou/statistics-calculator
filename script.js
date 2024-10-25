ceeae0ecf4ba61cbed3b30f9ba44ec6301a2313b
Malick Roungou <mjbroungou@gmail.com>
2024-10-24 20:38:24 -0400

    updates

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const calculate = () => {
     const numbers = array.map(el => Number(el)).filter(el => !isNaN(el))
 
     const mean = getMean(numbers);
+    const median = getMedian(numbers)
     document.querySelector('#mean').textContent = mean;
+    document.querySelector('#median').textContent = median;
 }
 
 // const getMean = (array) => {
@@ -19,3 +21,45 @@ const calculate = () => {
 // with implicit arrow functions
 
 const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
+
+const getMedian = (array) => {
+    const sorted = array.toSorted((a, b) => a - b)
+
+  /*   const isEven = sorted.length % 2 === 0
+    if (isEven) {
+        const firstMiddleNumber = sorted[sorted.length / 2]
+        const secondMiddleNumber = sorted[(sorted.length / 2) - 1]
+
+        const evenListMedian = getMean([firstMiddleNumber, secondMiddleNumber])
+        return evenListMedian
+    } else if (!isEven) {
+        const oddListMedian = sorted[Math.floor(sorted.length / 2)]
+        return oddListMedian
+    } */
+
+        // other way to write it using ternary operator
+
+    const median = 
+        sorted.length % 2 === 0 ? getMean([sorted[sorted.length / 2], sorted[sorted.length / 2 -1]])
+        : sorted[Math.floor(sorted.length / 2)]
+
+    return median
+}
+
+// // how to check if the length of an array is even or odd
+// const testArr1 = [1, 2, 3, 4, 5];
+// const testArr2 = [1, 2, 3, 4, 5, 6];
+// const isEven = testArr2.length % 2 === 0;
+// // to get the median with an odd number of elements, I need to find and return the middle number
+// /* using math.floor() because it returns a decimal as the array length is odd and rounding
+// down to find the nearest whole number */
+// const oddListMedian = testArr1[Math.floor(testArr1.length / 2)]
+
+// /* if the length of the array is even need to find the two middle numbers and get their
+// average */
+
+// const firstMiddleNumber = testArr2[testArr2.length / 2]
+// const secondMiddleNumber = testArr2[(testArr2.length / 2) - 1]
+
+// const evenListMedian = getMean([firstMiddleNumber, secondMiddleNumber])
+// console.log(evenListMedian)
